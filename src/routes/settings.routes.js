f600db342bf7a58458662b0d1fe729bf579061cb
Dom4002 <144023911+Dom4002@users.noreply.github.com>
2026-07-07 00:20:31 +0200

    Update settings.routes.js

--- src/routes/settings.routes.js
+++ src/routes/settings.routes.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middleware/auth.middleware');
-const roleMiddleware = require('../middleware/role.middleware');
+const { roleMiddleware } = require('../middleware/role.middleware');
 const {
   getAllSettings,
   getSettingsByCategory,
